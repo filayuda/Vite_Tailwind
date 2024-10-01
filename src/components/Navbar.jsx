e61e52cc67f8b9e4a68ff525147f3d1765d62f12
filayuda <filayudafirmansya@gmail.com>
2024-10-01 11:19:19 +0700

    AddHrefNavbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,48 +25,48 @@ const Navbar = () => {
     
   return (
     <div className={`navbar fixed w-full transition-all ${scrollActive}`}>
-      <div className="container mx-auto px-4">
-        <div className="navbar-box flex items-center justify-between">
+      <div className="container px-4 mx-auto">
+        <div className="flex items-center justify-between navbar-box">
           <div className="logo">
-            <h1 className="sm:text-2xl text-xl font-bold">Firman</h1>
+            <h1 className="text-xl font-bold sm:text-2xl">Firman</h1>
           </div>
           <ul
             className={`flex lg:gap-12 md:static md:flex-row md:shadow-none md:bg-transparent md:w-auto md:h-full md:translate-y-0 md:p-0 md:m-0 md:transition-none md:text-black gap-8 fixed ${menuActive} top-1/2 -translate-y-1/2 flex-col px-8 py-6 rounded shadow-lg shadow-slate-300 bg-sky-400 font-bold text-white transition-all`}
           >
             <li className="flex items-center gap-3">
-              <i className="ri-home-2-line text-2xl md:hidden block"></i>
-              <a href="#" className="font-medium opacity-75">
+              <i className="block text-2xl ri-home-2-line md:hidden"></i>
+              <a href="#home" className="font-medium opacity-75">
                 Beranda
               </a>
             </li>
             <li className="flex items-center gap-3">
-              <i className="ri-information-line text-2xl md:hidden block"></i>
-              <a href="#" className="font-medium opacity-75">
+              <i className="block text-2xl ri-information-line md:hidden"></i>
+              <a href="#about" className="font-medium opacity-75">
                 About Us
               </a>
             </li>
             <li className="flex items-center gap-3">
-              <i className="ri-settings-3-line text-2xl md:hidden block"></i>
-              <a href="#" className="font-medium opacity-75">
+              <i className="block text-2xl ri-settings-3-line md:hidden"></i>
+              <a href="#services" className="font-medium opacity-75">
                 Service
               </a>
             </li>
             <li className="flex items-center gap-3">
-              <i className="ri-image-line text-2xl md:hidden block"></i>
-              <a href="#" className="font-medium opacity-75">
+              <i className="block text-2xl ri-image-line md:hidden"></i>
+              <a href="#proyek" className="font-medium opacity-75">
                 Project
               </a>
             </li>
           </ul>
-          <div className="social flex items-center gap-2">
+          <div className="flex items-center gap-2 social">
             <a
-              href="#"
-              className="bg-sky-400 px-5 py-2 rounded-full text-white font-bold hover:bg-sky-700 transition-all"
+              href="#footer"
+              className="px-5 py-2 font-bold text-white transition-all rounded-full bg-sky-400 hover:bg-sky-700"
             >
               Social Media
             </a>
             <i
-              className="ri-menu-3-line text-3xl md:hidden block cursor-pointer"
+              className="block text-3xl cursor-pointer ri-menu-3-line md:hidden"
               onClick={handleClick}
             />
           </div>
